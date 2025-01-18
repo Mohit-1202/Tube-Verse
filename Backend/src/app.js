030f4855e9e57b43be2f09fa9f4f78ee4e4278ad
Xenon1202 <MOHITMISHRAK47@GMAIL.COM>
2025-01-18 23:09:49 +0530

    Register controller part-1

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import userRouter from './routes/user.routes.js'
 
 const app = express()
 
@@ -15,7 +16,7 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
-import userRouter from './routes/user.routes.js'
+
 app.use("/api/v1/users", userRouter)
 
 
