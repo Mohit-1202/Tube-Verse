38e9cbb1bff21059e5c86cdb2447a09d5f8cb7e7
Xenon1202 <MOHITMISHRAK47@GMAIL.COM>
2025-08-15 13:49:10 +0530

    Mock Videos Added

--- FrontEnd/src/Mocks/mockVideos.jsx
+++ FrontEnd/src/Mocks/mockVideos.jsx
@@ -26,5 +26,32 @@ export const mockVideos = [
     timestamp: "23-02-2022",
     thumbnail: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     duration: "5:45"
+  },
+  {
+    title: "Amazing Lord Ganesh Videos!!",
+    description: "This is a random description which is just made for this video and ...",
+    channel: "Shree Ganesh Bhajan",
+    views: "120M",
+    timestamp: "23-02-2022",
+    thumbnail: "https://images.unsplash.com/photo-1567878673047-0451c851056e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    duration: "10:25"
+  },
+  {
+    title: "!Vlog 22! Amazing day on river side",
+    description: "This is a random description which is just made for this video and ...",
+    channel: "A random Vlogger",
+    views: "1.2M",
+    timestamp: "23-02-2022",
+    thumbnail: "https://plus.unsplash.com/premium_photo-1675873580278-f5a6ea2a6f0a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHJpdmVyfGVufDB8fDB8fHww",
+    duration: "8:10"
+  },
+  {
+    title: "Top 5 upcoming laptops",
+    description: "This is a random description which is just made for this video and ...",
+    channel: "Techno Talks",
+    views: "228k",
+    timestamp: "23-02-2022",
+    thumbnail: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    duration: "5:45"
   }
 ];
